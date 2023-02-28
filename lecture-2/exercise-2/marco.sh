2dfdb7ce86c93b8f96827bd3ddee55c1180b96ff
Chilly-169-table <hemanthlvssr66@gmail.com>
2023-02-28 14:15:38 +0530

    Adding sh files for lec-2 ex-2

---
+++ lecture-2/exercise-2/marco.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+function marco(){
+	currentDirectory=$(pwd)
+	echo $currentDirectory > ~/missing/lecture-2/exercise-2/temp.txt
+}
---
+++ lecture-2/exercise-2/polo.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+#at	~/missing/lecture-2/exercise-2/temp.txt
+marcoDirectory=$(< ~/missing/lecture-2/exercise-2/temp.txt)
+cd $marcoDirectory
